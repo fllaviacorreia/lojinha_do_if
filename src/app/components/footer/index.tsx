5f3ae9e48a1fb9d39617f48b3df366fabeaa19c3
Flávia Correia <fllaviacorreia@outlook.com>
2024-12-10 11:28:19 -0300

    move to a folder the front

--- src/app/components/footer/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="flex items-center justify-center w-screen h-14 rounded-3xl absolute bottom-4 bg-slate-300">
-            <p className="font-semibold">Feito com ❤️ em Itapetinga - BA</p>
-        </footer>
-    )
-}
