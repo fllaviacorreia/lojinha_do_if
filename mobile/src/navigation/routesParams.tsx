41ef9b6187fe11b98229cf03ce492ce523b801fd
Flávia Correia <fllaviacorreia@outlook.com>
2024-12-10 11:30:30 -0300

    move to a folder the mobile

---
+++ mobile/src/navigation/routesParams.tsx
@@ -0,0 +1,5 @@
+// Define o tipo de objeto que contém os parâmetros das rotas
+export type RoutesParams = {
+    // A rota "Home" também não exige parâmetros e, portanto, é "undefined"
+    Home: undefined;
+};
